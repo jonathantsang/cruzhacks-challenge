db6a02aa246c296b508e742a411296c7b05d7a79
Bryan Ji <xj9797@gmail.com>
2019-03-06 00:15:29 -0800

    add error checking

--- app.py
+++ app.py
@@ -10,11 +10,16 @@ app = Flask(__name__)
 @app.route('/hackers', methods=['POST'])
 def hacker_insert():
     
+    if not (fr.values.get('UserTypeCode') or fr.values.get('Name') or fr.values.get('Street1') or fr.values.get('StateCode'), fr.values.get('ZipCode'), fr.values.get('CountryCode'), fr.values.get('Phone'), fr.values.get('Email'), fr.values.get('BirthDate')):
+        response = make_response(jsonify({'result':'Error', 'msg':'One or more required fields is missing'}), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
     handler = UserProfileHandler()
     profile = UserProfile(None, fr.values.get('UserTypeCode'), fr.values.get('Name'), fr.values.get('School'), fr.values.get('Major'), fr.values.get('Street1'), fr.values.get('Street2'), fr.values.get('City'), fr.values.get('StateCode'), fr.values.get('ZipCode'), fr.values.get('CountryCode'), fr.values.get('Phone'), fr.values.get('Email'), fr.values.get('BirthDate'), fr.values.get('ProfileImageUrl'))
     handler.insert(profile)
 
-    response = make_response(jsonify({'result':'Success', 'msg':'OK'}), 200)
+    response = make_response(jsonify({'result':'Success', 'msg':'OK'}), 201)
     response.headers['Content-Type'] = 'application/json'
     return response
 
@@ -43,6 +48,11 @@ def hacker_update(id):
 
     # we don't need to check if the id exists because we assume that this will be
     # handled client-side.
+
+    if not (fr.values.get('UserTypeCode') or fr.values.get('Name') or fr.values.get('Street1') or fr.values.get('StateCode'), fr.values.get('ZipCode'), fr.values.get('CountryCode'), fr.values.get('Phone'), fr.values.get('Email'), fr.values.get('BirthDate')):
+        response = make_response(jsonify({'result':'Error', 'msg':'One or more required fields is missing'}), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
     handler = UserProfileHandler()
     profile = UserProfile(id, fr.values.get('UserTypeCode'), fr.values.get('Name'), fr.values.get('School'), fr.values.get('Major'), fr.values.get('Street1'), fr.values.get('Street2'), fr.values.get('City'), fr.values.get('StateCode'), fr.values.get('ZipCode'), fr.values.get('CountryCode'), fr.values.get('Phone'), fr.values.get('Email'), fr.values.get('BirthDate'), fr.values.get('ProfileImageUrl'))
